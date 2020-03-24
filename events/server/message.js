f8893c423bebed7b0226d93b3658ee124e3f34e5
Joseph Mauro <49416852+too-mauro@users.noreply.github.com>
2020-03-23 20:49:53 -0400

    Adjustment to allow case-insensitivity

--- events/server/message.js
+++ events/server/message.js
@@ -6,16 +6,19 @@ module.exports = async (bot, message) => {
     if (message.author.bot || message.channel.type === "dm") return;
 
     // get the prefix from the server config file
-    var prefix = JSON.parse(fs.readFileSync(`./config/server/${message.guild.id}/config.json`)).prefix;
+    const prefix = JSON.parse(fs.readFileSync(`./config/server/${message.guild.id}/config.json`)).prefix;
 
     // if the message specifically pings the bot, return the server's prefix
     //if (message.isMentioned(bot.user)) return message.channel.send(`The prefix for this server is:\n\`${prefix}\``);
+    
+    // makes sure prefix is case-insensitive
+    let cleanPrefix = message.content.substr(0, prefix.length).toLowerCase();
+    if (cleanPrefix !== prefix) return;
 
     // otherwise, let it get any arguments and search for a known command
-    let args = message.content.slice(prefix.length).trim().split(/ +/g);
+    let args = message.content.slice(cleanPrefix.length).trim().split(/ +/g);
     let cmd = args.shift().toLowerCase();
 
-    if (!message.content.startsWith(prefix)) return;
-    let commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd))
-    if (commandfile) {commandfile.run(bot, message, args);}
+    let commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd));
+    if (commandfile) {  commandfile.run(bot, message, args);  }
 }
